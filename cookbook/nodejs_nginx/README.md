aed393aebd9cf7972e05d93179cb33a648ac3978
Elliot Harris <elliotharris@tcadmins-MacBook-Air.local>
2020-04-09 18:33:55 +0100

    working ami

---
+++ cookbook/nodejs_nginx/README.md
@@ -0,0 +1,44 @@
+# nodejs_nginx :squirrel:
+
+This cookbook will install Nodejs and Nginx.
+
+To use import it in your own cookbook and run it in the recipies.
+
+## Learning Outcomes
+- Git, GitHub
+- Documentation
+- Chef, Inspec, ChefSpec
+- Importing cookbooks
+- Attributes and Templates
+- TDD
+
+
+## Installation
+
+
+
+
+
+## Test
+
+To run the unit tests:
+
+`chef exec rspec`
+
+To run integration tests:
+`kitchen test`
+
+
+## ERB and Ice Cream Cones <% %> <%= =%>
+
+ERB is ebeded ruby.
+
+Meaning you can interpolate variable or run ruby code.
+
+It can then be converted into a string.
+
+ERB is like the f"this is a {<variable>}" but for entire documents!
+
+#### <% %> allow ruby with no output
+
+#### <%= =%> allows ruby with output (output is the same as print in python)
---
+++ cookbook/nodejs_nginx/recipes/default.rb
@@ -0,0 +1,49 @@
+#
+# Cookbook:: nodejs_nginx
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
+
+apt_update 'update_sources' do
+  action :update
+end
+
+package 'nginx' # do
+  # action [:install]
+  # ^^ default for package command - see nodejs
+# end
+
+
+apt_update 'update_sources' do
+  action :update
+end
+
+
+service 'nginx' do
+  action [:enable, :start]
+end
+
+template '/etc/nginx/sites-available/proxy.conf' do
+  source 'proxy.conf.erb'
+  variables proxy_port: node['nginx']['proxy_port']
+  notifies :restart, 'service[nginx]'
+end
+
+
+link '/etc/nginx/sites-enabled/proxy.conf' do
+  to '/etc/nginx/sites-available/proxy.conf'
+  notifies :restart, 'service[nginx]'
+end
+
+link '/etc/nginx/sites-enabled/default' do
+  action :delete
+  notifies :restart, 'service[nginx]'
+end
+
+
+include_recipe 'nodejs'
+
+# package 'npm'
+
+nodejs_npm 'pm2'
+nodejs_npm 'react'
